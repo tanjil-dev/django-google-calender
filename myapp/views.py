df74818fa3a8677c16e460bb6a7b3d22e4cf89f1
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2026-01-08 04:40:09 +0600

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        address = request.POST.get("address")
+        preferred_date = request.POST.get("preferred_date")
+        preferred_time = request.POST.get("preferred_time")
+        message = request.POST.get("message")
+
+        email_message = f"""
+        New Enquiry Received
+
+        Name: {name}
+        Email: {email}
+        Phone: {phone}
+        Address: {address}
+        Preferred Date: {preferred_date}
+        Preferred Time: {preferred_time}
+
+        Message:
+        {message}
+        """
+
+        send_mail(
+            subject="New Photography Enquiry",
+            message=email_message,
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[settings.DEFAULT_FROM_EMAIL],
+            fail_silently=False,
+        )
+
+        return redirect("index")
