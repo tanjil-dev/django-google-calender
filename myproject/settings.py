a99f948bf25083c132a32c3b14edc0976919feb5
Tanjil Ahmed Ovi <tanzil.ovi578@gmail.com>
2026-02-13 00:42:31 +0600

    debug true

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z+(-q_7hoq2kklwdw!ob#_xez3o7-+h(og=fcl^!wq9azf+^e('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'tanjilahmed.pythonanywhere.com']
 
